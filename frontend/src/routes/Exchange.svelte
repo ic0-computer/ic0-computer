c378e0d8e33cb6e96d5d5c3f308d53f263dfe53d
bradyinfinity <bradyleecarrico@outlook.com>
2024-02-10 22:18:02 -0500

    computr extension

--- frontend/src/routes/Exchange.svelte
+++ frontend/src/routes/Exchange.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
-import Loader from "../components/Loader.svelte";
-import { state } from "../libs/store";
-import { onMount } from "svelte";
+  import Loader from "../components/Loader.svelte";
+  import { state } from "../libs/store";
+  import { onMount } from "svelte";
 
-onMount(() => {
-  $state.page = "Exchange";
-})
+  function sendMessageToExtension() {
+    const requestInfo = { text: "hi you" };
+    window.postMessage({ source: "dataaccessgateway-agent", payload: requestInfo }, "*");
+  }
+
+  onMount(() => {
+    $state.page = "Exchange";
+  })
 </script>
 
 <h1 class="mt-6">Coming Soon!</h1>
 <div class="p-6">
   <Loader type="stretch" />
 </div>
+
+<button on:click={sendMessageToExtension}>Click Me</button>
